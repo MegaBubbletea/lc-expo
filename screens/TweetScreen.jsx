37d9ca8df7879d96083341751dc44ea7d6bf030d
Diluen Cheong <diluen@outlook.com>
2022-09-15 20:31:47 +0900

    [Section 2 - Episode 8] - Implement Design for New Tweet Screen

--- screens/TweetScreen.jsx
+++ screens/TweetScreen.jsx
@@ -9,11 +9,15 @@ import {
 } from "react-native";
 import { Entypo, EvilIcons } from "@expo/vector-icons";
 
-export default function TweetScreen() {
+export default function TweetScreen({ navigation }) {
+  function gotoProfile() {
+    navigation.navigate("Profile Screen");
+  }
+
   return (
     <View style={styles.container}>
       <View style={styles.profileContainer}>
-        <TouchableOpacity style={styles.flexRow}>
+        <TouchableOpacity style={styles.flexRow} onPress={() => gotoProfile()}>
           <Image
             style={styles.avatar}
             source={{
