a29051bfc10f68a258341248047e3c71452840c8
Diluen Cheong <diluen@outlook.com>
2022-09-18 13:22:00 +0900

    [Section 3 - Episode 19] - Allow users to register

--- screens/Auth/RegisterScreen.jsx
+++ screens/Auth/RegisterScreen.jsx
@@ -9,6 +9,7 @@ import {
   Alert,
   Image,
 } from "react-native";
+import axiosConfig from "../../helpers/axiosConfig";
 
 export default function RegisterScreen({ navigation }) {
   const [name, setName] = useState("");
@@ -20,7 +21,33 @@ export default function RegisterScreen({ navigation }) {
   const [error, setError] = useState(null);
 
   function register(email, username, password, confirmPassword) {
-    Alert.alert("Register Logic here");
+    if (name.length < 1) {
+      Alert.alert("Please enter a name");
+      return;
+    }
+
+    setIsLoading(true);
+    axiosConfig
+      .post("/register", {
+        name,
+        email,
+        username,
+        password,
+        password_confirmation: confirmPassword,
+      })
+      .then((response) => {
+        Alert.alert("User created! Please login.");
+        navigation.navigate("Login Screen");
+
+        setIsLoading(false);
+        setError(null);
+      })
+      .catch((error) => {
+        console.log(error.response);
+        const key = Object.keys(error.response.data.errors)[0];
+        setError(error.response.data.errors[key][0]);
+        setIsLoading(false);
+      });
   }
 
   return (
